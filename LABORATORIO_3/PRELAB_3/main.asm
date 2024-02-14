64d5bd3a85d0292cb806da46ca6e7fc7fb6de74c
Javier-Vinals <vin22619@uvg.edu.gt>
2024-02-14 13:34:19 -0600

    Update main.asm

--- LABORATORIO_3/PRELAB_3/main.asm
+++ LABORATORIO_3/PRELAB_3/main.asm
@@ -8,35 +8,43 @@
 ;******************************************************************************
 ;Encabezado
 ;******************************************************************************
-.include "M328PDEF.inc"
-.cseg
+.include "M328PDEF.INC"
+.cseg 
 .org 0x00
 	JMP MAIN
-.org 0x02
-	JMP INCC1
-.org 0X04
-	JMP DECC1
-;******************************************************************************
-;Stack
-;******************************************************************************
+
+.org 0x0002
+	JMP ISR_INT0
+
+.org 0x0004	
+	JMP ISR_INT1
+
+
 MAIN:
-LDI R16, LOW(RAMEND)
-OUT SPL, R16
-LDI R21, HIGH(RAMEND)
-OUT SPH, R17
 ;******************************************************************************
-; Configuracion
+;Stack pointer
 ;******************************************************************************
+	LDI R16, LOW(RAMEND)
+	OUT SPL, R16
+	LDI R17, HIGH(RAMEND)
+	OUT SPH, R17 
+
+
 Setup:
+;Establece los pines como pullup
+	SBI PORTD, PD2
+	SBI PORTD, PD3
+;Establece los puertos de entrada y salida
+	CBI DDRD, PD2
+	CBI DDRD, PD3
 
-//boton 1 C1
-	CBI DDRD, PD2 ; configuracion PD2 como entrada
-	SBI PORTD, PD2	;Colocar PD2 como pullup
-//boton 2 C1
-	CBI DDRD, PD3 ; configuracion PD3 como entrada
-	SBI PORTD, PD3	;Colocar PD3 como pullup
+	LDI R18, (1 << ISC01)|(1 << ISC00)|(1 << ISC11)|(1 << ISC10)
+	STS EICRA, R16
 
-//Contador 1
+	SBI EIMSK, INT0
+	SBI EIMSK, INT1
+
+
 	SBI DDRB, PB5//mas significativo
 	CBI PORTB, PB5
 	SBI DDRB, PB4
@@ -46,20 +54,18 @@ Setup:
 	SBI DDRB, PB2 // menos significativo
 	CBI PORTB, PB2
 
-LDI R16, (1 << ISC11)|(1 << ISC10)|(1 << ISC01)|(1 << ISC00)
-STS EICRA, R16
+	SEI
 
-SBI EIMSK, INT0
-SBI EIMSK, INT1
+Loop:
+	RJMP Loop 
 
-SEI
+;******************************************************************************
+;Subrutinas
+;******************************************************************************
 
-	//CONDICIONALES
-
-LOOP:
-
-
-	//LEDS ON
+	
+LEDS:
+;Verificamos el registro 20 bit por bit para encender cada led
 	SBRC R21, 0		; BIT 0 de R21 EN 1
 	SBI PORTB, 2  ; LED EN PUERTO D2
 	SBRC R21, 1		; BIT 1
@@ -78,30 +84,38 @@ LOOP:
 	CBI PORTB, 4  ; PUERTO D4
 	SBRS R21, 3		; BIT 3
 	CBI PORTB, 5  ; PUERTO D5
+	RET	
+	
+ISR_INT0:
+;Guardamos el R16 en la pila para no perderla
+	PUSH R16
+	IN R16, SREG 
+	PUSH R16
 
-	RJMP LOOP
+;Colocar el codigo que queremos aqui 
+	INC R21
+	CALL LEDS
+
+	POP R16
+	OUT SREG, R16
+	POP R16
+	RETI
+
+ISR_INT1:
+;Guardamos el R16 en la pila para no perderla
+	PUSH R16
+	IN R16, SREG 
+	PUSH R16
+
+;Colocar el codigo que queremos aqui 
+	DEC R21
+	CALL LEDS
+;Sacar el r16 de la pila
+	POP R16
+	OUT SREG, R16
+	POP R16
+	RETI
+
+	
 
 
-INCC1:
-PUSH R16
-IN R16, SREG
-PUSH R16
-
-INC R21
-
-POP R16
-OUT SREG, R16
-POP R16
-RETI
-
-DECC1:
-PUSH R16
-IN R16, SREG
-PUSH R16
-
-DEC R21
-
-POP R16
-OUT SREG, R16
-POP R16
-RETI
